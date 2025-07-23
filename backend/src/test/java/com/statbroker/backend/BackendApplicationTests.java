edd0c1e480a3669be15ca0120f17bbb51c265f83
AGALMAD <agalmad@g.educaand.es>
2025-07-23 19:30:38 +0200

    Initialize Spring Boot backend project

---
+++ backend/src/test/java/com/statbroker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statbroker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
