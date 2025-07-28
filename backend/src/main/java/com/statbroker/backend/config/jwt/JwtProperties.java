4316ae2b4c6d55b7f14af321dfc57b15aa1589b0
AGALMAD <agalmad@g.educaand.es>
2025-07-28 20:37:50 +0200

    Add JWT authentication and configuration

---
+++ backend/src/main/java/com/statbroker/backend/config/jwt/JwtProperties.java
@@ -0,0 +1,12 @@
+package com.statbroker.backend.config.jwt;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private String key;
+    private Long accessTokenExpiration;
+    private Long refreshTokenExpiration;
+}
